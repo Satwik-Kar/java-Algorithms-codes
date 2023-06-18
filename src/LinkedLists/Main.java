e5386578ca5f5f18b9b58806dce63232804e7b3c
Satwik Kar <satwik.k.2000@gmail.com>
2023-06-18 23:38:50 +0530

    refinements

--- src/LinkedLists/Main.java
+++ src/LinkedLists/Main.java
@@ -36,21 +36,21 @@ public class Main {
                     System.out.println("(1). Type Integer\n(2). Type Double\n(3). Type Float\n(4). Type String");
                     int choice_ = sc.nextInt();
                     if (choice_ == 1) {
-                        linkedList = createLinkedList("integer");
+                        linkedList = new LL<Integer>();
                         TYPE_CHOICE = 1;
                         System.out.println("LinkedList created successfully.");
                     } else if (choice_ == 2) {
-                        linkedList = createLinkedList("double");
+                        linkedList = new LL<Double>();
                         TYPE_CHOICE = 2;
                         System.out.println("LinkedList created successfully.");
 
                     } else if (choice_ == 3) {
-                        linkedList = createLinkedList("float");
+                        linkedList = new LL<Float>();
                         TYPE_CHOICE = 3;
                         System.out.println("LinkedList created successfully.");
 
                     } else if (choice_ == 4) {
-                        linkedList = createLinkedList("string");
+                        linkedList = new LL<String>();
                         TYPE_CHOICE = 4;
                         System.out.println("LinkedList created successfully.");
 
@@ -62,23 +62,24 @@ public class Main {
 
                     } else {
                         System.out.println("Enter an element according to the your given type of LinkedList:");
+                        Object data = 0;
                         if (TYPE_CHOICE == 1) {
-                            int data = sc.nextInt();
+                            data = sc.nextInt();
                             linkedList.add(data);
                             System.out.println("Element successfully inserted.");
 
                         } else if (TYPE_CHOICE == 2) {
-                            double data = sc.nextDouble();
+                            data = sc.nextDouble();
                             linkedList.add(data);
                             System.out.println("Element successfully inserted.");
 
                         } else if (TYPE_CHOICE == 3) {
-                            float data = sc.nextFloat();
+                            data = sc.nextFloat();
                             linkedList.add(data);
                             System.out.println("Element successfully inserted.");
 
                         } else if (TYPE_CHOICE == 4) {
-                            String data = sc.next();
+                            data = sc.next();
                             linkedList.add(data);
                             System.out.println("Element successfully inserted.");
 
@@ -136,13 +137,4 @@ public class Main {
         }
     }
 
-    public static LL<?> createLinkedList(String type) {
-        return switch (type) {
-            case "integer" -> new LL<Integer>();
-            case "string" -> new LL<String>();
-            case "float" -> new LL<Float>();
-            case "double" -> new LL<Double>();
-            default -> null;
-        };
-    }
 }
