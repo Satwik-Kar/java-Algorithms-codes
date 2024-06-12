a0f0a1500da638f53f5e17f1081ec71482c0b4ad
Satwik Kar <satwik.k.2000@gmail.com>
2024-06-12 11:19:14 +0530

    added GraphStoring.java

--- src/Revision/GraphStoring.java
+++ src/Revision/GraphStoring.java
@@ -1,12 +1,10 @@
 package Revision;
 
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 
 //adjacency list
-public class BinarySearchTree {
+public class GraphStoring {
     static class AdjacencyList{
         int noOfVertices;
         static ArrayList<Integer>[] adjacencyList;
